95b980726384b6cb5756e5fa4b9c0c7628f4f7ea
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-08-25 08:38:21 -0300

    feat: add StateBlock in TimeBlock model

--- src/models/time_block.rs
+++ src/models/time_block.rs
@@ -1,12 +1,20 @@
 use chrono::{DateTime, Duration, Local};
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum StateBlock {
+    InProgress,
+    Paused,
+    Finished,
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct TimeBlock {
     pub durantion: Duration,
     pub message: Option<String>,
     pub start_time: DateTime<Local>,
     pub end_time: DateTime<Local>,
+    pub state: StateBlock,
 
     pub denied_acess: Vec<String>,
     pub allow_acess: Vec<String>,
