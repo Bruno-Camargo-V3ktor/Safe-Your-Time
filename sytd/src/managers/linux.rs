14b500a3a6c871161b2a0f8df0bb53a87f122b51
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-09-29 16:00:15 -0300

    feat: Linux Manager implamation get user active session

--- sytd/src/managers/linux.rs
+++ sytd/src/managers/linux.rs
@@ -1,3 +1,4 @@
+use anyhow::{Ok, anyhow};
 use tokio::process::Command;
 
 use super::Manager;
@@ -6,7 +7,11 @@ pub struct LinuxManager {}
 
 impl Manager for LinuxManager {
     async fn monitoring_apps(&self, apps: Vec<String>) {
-        let output = Command::new("top").args(["-b", "-n", "1"]).output().await.unwrap();
+        let output = Command::new("top")
+            .args(["-b", "-n", "1"])
+            .output()
+            .await
+            .unwrap();
 
         if output.status.success() {
             let stdout = String::from_utf8_lossy(&output.stdout);
@@ -27,13 +32,32 @@ impl Manager for LinuxManager {
     }
 
     async fn kill_process(&self, id_process: String) -> anyhow::Result<()> {
-        Command::new("kill").args(["-9", &id_process]).status().await?;
+        Command::new("kill")
+            .args(["-9", &id_process])
+            .status()
+            .await?;
 
         Ok(())
     }
 
     async fn get_username(&self) -> anyhow::Result<String> {
-        todo!()
+        let output = Command::new("loginctl")
+            .args(["list-users", "-P", "state=active", "--no-legend"])
+            .output()
+            .await?;
+
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+
+            for line in stdout.lines() {
+                let parts: Vec<&str> = line.split_whitespace().collect();
+                let username = parts[1].to_string();
+
+                return Ok(username);
+            }
+        }
+
+        Err(anyhow!("{:?}", String::from_utf8(output.stderr)))
     }
 
     async fn notification(&self, title: String, body: String) -> anyhow::Result<()> {
